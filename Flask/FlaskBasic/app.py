802e1451ab449e73701a35b1235610fe470963ef
unknown <shubhamsinghal12@gmail.com>
2025-03-11 12:12:26 +0530

    'Flask'

--- Flask/FlaskBasic/app.py
+++ Flask/FlaskBasic/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -12,8 +12,22 @@ def start():
 
 @app.route('/home')
 def home():
-    return render_template('home.html',name='Shubham',arr=[i for i in range(10,20)])
+    # print(request.args)
+    name = request.args.get('name')
+    if name == None:
+        name = 'Guest'
+    return render_template('home.html',name=name,arr=[11,22,33,44,55,66,77])
 
+
+@app.route('/form',methods = ['GET','POST'])
+def form():
+    if request.method == 'GET':
+        return render_template('form.html')
+    else:
+        # print(request.form)
+        digit_file = request.files.get('digit')
+        digit_file.save('./file.png')
+        return f"<html><head></head><body><h1>Thank you {request.form.get('fname')} </h1></body></html>"
 
 
 if __name__ == '__main__':
